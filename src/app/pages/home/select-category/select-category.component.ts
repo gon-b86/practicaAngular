4886854238336fd461aa566a12df109b7818da36
Gonzalo <belmonte86@gmail.com>
2025-01-31 21:24:04 +0100

    componentes, servicio e interfaces creados

---
+++ src/app/pages/home/select-category/select-category.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-category',
+  imports: [],
+  templateUrl: './select-category.component.html',
+  styleUrl: './select-category.component.css'
+})
+export class SelectCategoryComponent {
+
+}
---
+++ src/app/pages/post-new/post-new.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-new',
+  imports: [],
+  templateUrl: './post-new.component.html',
+  styleUrl: './post-new.component.css'
+})
+export class PostNewComponent {
+
+}
