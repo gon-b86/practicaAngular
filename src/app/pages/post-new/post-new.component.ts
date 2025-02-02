8dc4b77095e563f7bf0d3d55fa2dc53b5056d0cd
Gonzalo <belmonte86@gmail.com>
2025-02-02 15:11:47 +0100

    Form creado, a falta de buen funcionamiento del Submit

--- src/app/pages/post-new/post-new.component.ts
+++ src/app/pages/post-new/post-new.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-post-new',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './post-new.component.html',
   styleUrl: './post-new.component.css'
 })
 export class PostNewComponent {
+  formModel: FormGroup;
+  postsService = inject(PostsService);
+
+  constructor() {
+    this.formModel = new FormGroup({
+      title: new FormControl("",[
+        Validators.required,
+      ]),
+      text: new FormControl("",[
+        Validators.required,
+      ]),
+      author: new FormControl("",[
+        Validators.required,
+      ]),
+      image: new FormControl("",[
+        Validators.required,
+      ]),
+      date: new FormControl("",[
+        Validators.required
+      ]),
+      category: new FormControl("",[
+        Validators.required,
+        Validators.minLength(3)
+      ])
+    })
+  }
+
+  checkErrorField(field: string, error: string): boolean {
+    if (this.formModel.get(field)?.hasError(error) && this.formModel.get(field)?.touched) {
+      return true;
+    }
+    return false;
+  }
+
+  onSubmit() {
+    if (this.formModel.valid) {
+      this.postsService.insert(this.formModel.value);
+      this.formModel.reset();
+      alert("Post añadido satisfactoriamente");
+      console.log(this.postsService.getAll());
+    }
+  }
 
 }
